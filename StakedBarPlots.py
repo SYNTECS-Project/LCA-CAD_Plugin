11efb3a9648bf24cfc77abbd2a3f9469a83aaa95
Jéssica Corujeira <117850894+JCoruja@users.noreply.github.com>
2026-01-23 15:19:41 +0000

    Implement Y-axis limit adjustment in graph plotting
    Added a method to round up values for Y-axis limits.

--- StakedBarPlots.py
+++ StakedBarPlots.py
@@ -12,6 +12,7 @@ Keys = namedtuple('Keys', ['FACTOR', 'PHASES', 'FABMETHOD'])
 keys = Keys(FACTOR='factor', PHASES='phases', FABMETHOD='fabmethod')
 
 class StakedBar_Graph_Class():    
+
     def CreateStakedBarGraphPlot(lca_Data, graphWidget):
         if not lca_Data:
             return graphWidget
@@ -34,6 +35,9 @@ class StakedBar_Graph_Class():
                 i=0
                 graphWidget.addLabel(text=factor[1], size='10pt', bold=True, colspan=len(phases)) # Set line title with impact factor name
                 graphWidget.nextRow()
+
+                yVal = max(data.values())
+                maxY = StakedBar_Graph_Class.round_up_to_nearest_int(yVal[0])
                 
                 for fabmethod in data.keys():
                     barPlot = graphWidget.addPlot() #(title=factor[1]) # Set plot title with impact factor name
@@ -46,6 +50,7 @@ class StakedBar_Graph_Class():
                     #barPlot.getAxis('bottom').setTicks(([(j*barWidth + barWidth, fabmethod) for j, fabmethod in enumerate(data.keys())], [])) # Set X axis ticks with fabrication methods
                     barPlot.showAxis('bottom', False)  # Show X axis
                     #barPlot.enableAutoRange('xy', True)  # Enable auto range for both axes
+                    barPlot.setYRange(0, maxY)
                     
                     values = data.get(fabmethod)
                     bottom = 0
@@ -63,5 +68,8 @@ class StakedBar_Graph_Class():
                 graphWidget.nextRow()        
             
             return graphWidget
+
+    def round_up_to_nearest_int(num):
+        return math.ceil(num)
 # https://stackoverflow.com/questions/70435112/pyqtgraph-stacked-bar-graph
 # https://doc.qt.io/qt-6/qtcharts-overview.html
